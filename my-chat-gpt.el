e835cbcd999fe0b013e34c32a9689863826cb248
Joakim G. <jocke@gmail.com>
2025-09-11 17:01:49 +0200

    first version of chat-tools

--- my-chat-gpt.el
+++ my-chat-gpt.el
@@ -1,5 +1,19 @@
-;; Your existing gptel setup (backend/model) here, e.g.:
-
+;; Author: 2025 Joakim G.
+;; URL:
+;; Version: 0.1
+;; Keywords: convenience, tools, ai, gpt
+;; License: MIT
+;; Description: Simple ChatGPT code assistant using gptel
+;; 2025-09-11 Initial version
+;;
+;; Installation:
+;; * Add this file to your Emacs load-path and add the following to your init file (e.g. ~/.emacs or ~/.emacs.d/init.el):
+;; * Make sure you have set your OpenAI API key in ~/.authinfo
+;;   Example entry in ~/.authinfo:
+;;   machine api.openai.com login apikey password sk-proj-***
+;; * You can get an API key from https://platform.openai.com/account/api-keys
+;;
+;; How to use:
 ;; (require 'my-chat-gpt)
 ;; (global-set-key (kbd "C-c g") #'gptel-change-region)
 ;; (global-set-key (kbd "C-c r") #'gptel-review-region)
